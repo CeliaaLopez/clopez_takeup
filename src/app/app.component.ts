1ddc1c478088e994f7368f4e2213cb3d9dd6767a
Celia López <45celia45@gmail.com>
2025-01-27 13:21:52 +0100

    exclusivo hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
+import { AdvantagesComponent } from './advantages/advantages.component';
 @Component({
   selector: 'app-root',
-  imports: [NavComponent, HeaderComponent],
+  imports: [NavComponent, HeaderComponent,AdvantagesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
