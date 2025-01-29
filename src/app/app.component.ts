e9962e41272f4ec67f7fa9bd62d7a6e894b4840a
Celia López <45celia45@gmail.com>
2025-01-29 14:14:49 +0100

    cambios 29 enero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { NavComponent } from './nav/nav.component';
-import { HeaderComponent } from './header/header.component';
-import { AdvantagesComponent } from './advantages/advantages.component';
 @Component({
   selector: 'app-root',
-  imports: [NavComponent, HeaderComponent,AdvantagesComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
